ec06b08f4a1091a2d80cc0206db36af7c5b0b9da
Stanley Lee <sachih@live.com>
2020-02-17 06:49:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -89,17 +89,7 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   
-  array = []
-  i = 0 
-  while i < source.length 
-    director_name = source[i][:name]
-    director_movies = source[i][:movies]
-    
-    array.push(movies_with_director_key(director_name, director_movies))
-    i += 1
-  end 
-  array
-end
+  
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
