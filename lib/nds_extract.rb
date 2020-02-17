5609cf6f47844943165894616335e28c14c2b1ef
Stanley Lee <sachih@live.com>
2020-02-17 07:01:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -104,7 +104,7 @@ def movies_with_directors_set(source)
   i = 0 
   while i < source.length do 
     name = source[i][:name]
-    array.push()
+    array.push(movies_with_director_key)
 end
 
 # ----------------    End of Your Code Region --------------------
